95f07b20bdcc605a9d62408a7d5dbc7ee76a6d30
Lukas Horak <lukas.horak@gmail.com>
2022-03-14 20:19:27 +0100

    Add java maven projekt

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+
+    public int sum(int a, int b){
+        return a+b;
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("Ahoj, jak se mas?");
+        System.out.println("Mam se dobre!");
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testSumOk(){
+
+        Main main = new Main();
+        main.sum(2, 5);
+        //throw new RuntimeException("Chyba");
+        assertEquals(7, main.sum(2, 5));
+
+    }
+
+    @Test
+    public void testSumFail(){
+
+        Main main = new Main();
+        main.sum(2, 5);
+        assertEquals(8, main.sum(2, 6));
+    }
+
+}
